39ee1ef42633492f9738057e5f16ed7c045511a9
Makoto Nishimura <mac.nishimura@gmail.com>
2016-06-10 15:52:03 +0900

    プロジェクト ファイルを追加します。

---
+++ C1PdfDocumentSample/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace C1PdfDocumentSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c1PdfDocument1 = new C1.C1Pdf.C1PdfDocument();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c1PdfDocument1
+            // 
+            this.c1PdfDocument1.DocumentInfo.Author = "";
+            this.c1PdfDocument1.DocumentInfo.CreationDate = new System.DateTime(((long)(0)));
+            this.c1PdfDocument1.DocumentInfo.Creator = "";
+            this.c1PdfDocument1.DocumentInfo.Keywords = "";
+            this.c1PdfDocument1.DocumentInfo.Producer = "ComponentOne C1Pdf";
+            this.c1PdfDocument1.DocumentInfo.Subject = "";
+            this.c1PdfDocument1.DocumentInfo.Title = "";
+            this.c1PdfDocument1.MaxHeaderBookmarkLevel = 0;
+            this.c1PdfDocument1.RotateAngle = 0F;
+            this.c1PdfDocument1.UseFastTextOut = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "http://coelacanth.heteml.jp/blog/wp-content/uploads/2013/08/cropped-icon.png";
+            this.pictureBox1.Location = new System.Drawing.Point(33, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "PDFを保存する";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private C1.C1Pdf.C1PdfDocument c1PdfDocument1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C1PdfDocumentSample/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C1PdfDocumentSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.label1.Text = "「PDFを保存する」をクリックすると\n画像付きのPDFを\n" + System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\sample.pdf\nに保存します";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // PDFに文字を表示する
+            this.c1PdfDocument1.DrawString("PDF Sample", new Font(FontFamily.GenericSerif, 16), Brushes.Black, PointF.Empty);
+
+
+            // 画像を表示する
+            RectangleF rect = c1PdfDocument1.PageRectangle;
+            rect.Inflate(-120, -240);
+
+            this.c1PdfDocument1.DrawImage(pictureBox1.Image, rect);
+
+            // 保存する
+            this.c1PdfDocument1.Save(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\sample.pdf");
+        }
+    }
+}
